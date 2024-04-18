50e097a3feac9fa2d6b947c197502ab59b0b165f
Vlad-Stefan Sas <me@svs.pm>
2024-04-18 15:30:05 +0300

    feat: added Scene class

--- SMMG3D - Tancodrom/SMMG3D - Tancodrom/Scene.cpp
+++ SMMG3D - Tancodrom/SMMG3D - Tancodrom/Scene.cpp
@@ -1,1 +1,58 @@
+#include "Program.h"
 #include "Scene.h"
+
+std::vector<SceneObject*> Scene::m_objects;
+Camera* Scene::m_camera;
+float Scene::m_deltaTime;
+
+void Scene::Start()
+{
+	m_camera = new Camera(glm::vec3(0.5, 0.5, 10), glm::vec3(0, 0, 0));
+	m_objects.push_back(m_camera);
+}
+
+void Scene::Run()
+{
+	float lastFrame = 0;
+	m_deltaTime = 0;
+
+	while (true)//TODO:  while (!glfwWindowShouldClose(window)) de preferat in Program.cpp
+	{
+		float currentFrame = glfwGetTime();
+		m_deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		for (auto object : m_objects)
+		{
+			object->Update();
+		}
+
+		glfwSwapBuffers(Program::GetWindow());
+		glfwPollEvents();
+	}
+}
+
+void Scene::Instantiate(SceneObject* object)
+{
+	m_objects.push_back(object);
+}
+
+glm::vec3 Scene::Forward()
+{
+	return glm::vec3(0.0f, 0.0f, -1.0f);
+}
+
+glm::vec3 Scene::Right()
+{
+	return glm::vec3(1.0f, 0.0f, 0.0f);
+}
+
+glm::vec3 Scene::Up()
+{
+	return glm::vec3(0.0f, 1.0f, 0.0f);
+}
+
+float Scene::GetDeltaTime()
+{
+	return m_deltaTime;
+}
--- SMMG3D - Tancodrom/SMMG3D - Tancodrom/Scene.h
+++ SMMG3D - Tancodrom/SMMG3D - Tancodrom/Scene.h
@@ -1,6 +1,30 @@
 #pragma once
 
+#include <vector>
+#include <GLFW/glfw3.h>
+
+#include "SceneObject.h"
+#include "Camera.h"
+
 class Scene
 {
+public:
+	static void Start();
+
+	static void Run();
+
+	static void Instantiate(SceneObject* object);
+
+	static float GetDeltaTime();
+
+	static glm::vec3 Forward();
+	static glm::vec3 Right();
+	static glm::vec3 Up();
+
+private:
+	static std::vector<SceneObject*> m_objects;
+	static Camera* m_camera;
+
+	static float m_deltaTime;
 };
 
