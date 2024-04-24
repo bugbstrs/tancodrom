5fbe37498951398f0ee6240de96f798f44e27244
Vlad-Stefan Sas <me@svs.pm>
2024-04-24 20:55:56 +0300

    feat: Added Model

---
+++ SMMG3D - Tancodrom/SMMG3D - Tancodrom/Model.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include "Mesh.h"
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+class Model
+{
+public:
+    // constructor, expects a filepath to a 3D model.
+    Model(std::string const& path, bool gamma = false);
+
+    // draws the model, and thus all its meshes
+    void Draw(Shader& shader);
+private:
+    std::vector<Texture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+    std::vector<Mesh> meshes;
+    std::string directory;
+    bool gammaCorrection;
+
+    void loadModel(std::string const& path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+};
+
+unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma);
