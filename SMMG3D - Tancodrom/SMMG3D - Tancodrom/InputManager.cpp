ca9954adcd89de2e0656b09594d12e3e5351f1e5
tdennis <teglasdennis@yahoo.com>
2024-05-07 16:36:58 +0300

    [InputManager]

--- SMMG3D - Tancodrom/SMMG3D - Tancodrom/InputManager.cpp
+++ SMMG3D - Tancodrom/SMMG3D - Tancodrom/InputManager.cpp
@@ -1,7 +1,50 @@
 #include "InputManager.h"
 #include "Program.h"
 
+float InputManager::m_mouseChangedX = 0;
+float InputManager::m_mouseChangedY = 0;
+float InputManager::m_oldMouseX = 0;
+float InputManager::m_oldMouseY = 0;
+float InputManager::m_scrollY = 0;
+bool InputManager::m_mouseMoved = false;
+
 bool InputManager::KeyDown(int key)
 {
     return glfwGetKey(Program::GetWindow(), key) == GLFW_PRESS;
+}
+
+float InputManager::MouseMoveX()
+{
+    if (!m_mouseMoved) return 0;
+    return m_mouseChangedX;
+}
+
+float InputManager::MouseMoveY()
+{
+    if (!m_mouseMoved) return 0;
+    return m_mouseChangedY;
+}
+
+float InputManager::ScrollY()
+{
+    return m_scrollY;
+}
+
+void InputManager::MouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    m_mouseMoved = true;
+    m_mouseChangedX = xpos - m_oldMouseX;
+    m_mouseChangedY = m_oldMouseY - ypos;
+    m_oldMouseX = xpos;
+    m_oldMouseY = ypos;
+}
+
+void InputManager::ClearMouseMovement()
+{
+    m_mouseMoved = false;
+}
+
+void InputManager::ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    m_scrollY = yoffset;
 }
--- SMMG3D - Tancodrom/SMMG3D - Tancodrom/InputManager.h
+++ SMMG3D - Tancodrom/SMMG3D - Tancodrom/InputManager.h
@@ -1,11 +1,31 @@
 #pragma once
+#include "Program.h"
+#include "Scene.h"
 
 class InputManager
 {
 public:
 	static bool KeyDown(int key);//ex key: GLFW_KEY_W
 
-private:
+	static float MouseMoveX();
+	static float MouseMoveY();
 
+	static float ScrollY();
+
+private:
+	static float m_mouseChangedX;
+	static float m_mouseChangedY;
+	static float m_oldMouseX;
+	static float m_oldMouseY;
+	static bool  m_mouseMoved;
+
+	static float m_scrollY;
+
+	static void MouseCallback(GLFWwindow* window, double xpos, double ypos);
+	static void ClearMouseMovement();
+	static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+
+	friend class Program;
+	friend class Scene;
 };
 
