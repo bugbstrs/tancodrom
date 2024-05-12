3eddcfbbb7b89dda6f0d187245c078a56c201f3d
tdennis <teglasdennis@yahoo.com>
2024-05-12 22:49:58 +0300

    Shaders work with multiple lights

--- Fragment Shaders/ShadowMapping.fs
+++ Fragment Shaders/ShadowMapping.fs
@@ -1,6 +1,14 @@
 #version 330 core
 out vec4 FragColor;
 
+#define MAX_LIGHTS 20
+
+struct Light {
+    vec3 position;
+    vec3 color;
+    float intensity;
+};
+
 in VS_OUT {
     vec3 FragPos;
     vec3 Normal;
@@ -11,11 +19,9 @@ in VS_OUT {
 uniform sampler2D diffuseTexture;
 uniform sampler2D shadowMap;
 
-uniform vec3 lightPos;
 uniform vec3 viewPos;
-
-uniform float hue;
-
+uniform int numLights;
+uniform Light lights[MAX_LIGHTS];
 
 float ShadowCalculation(vec4 fragPosLightSpace)
 {
@@ -29,9 +35,9 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     float currentDepth = projCoords.z;
     // calculate bias (based on depth map resolution and slope)
     vec3 normal = normalize(fs_in.Normal);
-    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    vec3 lightDir = normalize(lights[0].position - fs_in.FragPos);
 
-    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.001);
+    float bias = max(0.001 * (1.0 - dot(normal, lightDir)), 0.001);
     float shadow = 0.0;
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
     for (int x = -1; x <= 1; ++x)
@@ -44,7 +50,7 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     }
       shadow /= 9.0;
 
-     if (projCoords.z > 1.0)
+     if (projCoords.z > 0.99f)
         shadow = 0.0f;
 
     return shadow;
@@ -52,26 +58,38 @@ float ShadowCalculation(vec4 fragPosLightSpace)
 
 void main()
 {           
-    vec3 color = hue * texture(diffuseTexture, fs_in.TexCoords).rgb;
+    vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
     vec3 normal = normalize(fs_in.Normal);
-    vec3 lightColor = vec3(0.3);
-    // ambient
-    vec3 ambient = 0.7 * color;
-    // diffuse
-    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
-    float diff = max(dot(lightDir, normal), 0.0);
-    vec3 diffuse = diff * lightColor;
-    // specular
-    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = 0.0;
-    vec3 halfwayDir = normalize(lightDir + viewDir);  
-    spec = pow(max(dot(normal, halfwayDir), 0.0), 4096.0);
-    vec3 specular = spec * lightColor;    
-    
-    // calculate shadow
-    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
-    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
-    
+    vec3 lighting = vec3(0.0);
+
+    if(length(vec3(0.0) - fs_in.FragPos) > 150)
+    {
+        FragColor = vec4(color, 1.0);
+        return;
+    }
+
+    for (int i = 0; i < numLights; ++i)
+    {
+        // intensity
+        float distance = length(lights[i].position - fs_in.FragPos);
+        float intensity = lights[i].intensity / log(distance * distance + 1.0);
+        // ambient
+        vec3 ambient = 0.3 * lights[i].color * intensity;
+        // diffuse
+        vec3 lightDir = normalize(lights[i].position - fs_in.FragPos);
+        float diff = max(dot(lightDir, normal), 0.0);
+        vec3 diffuse = diff * lights[i].color * intensity;
+        // specular
+        vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+        float spec = 0.0;
+        vec3 halfwayDir = normalize(lightDir + viewDir);
+        spec = pow(max(dot(normal, halfwayDir), 0.0), 4096.0);
+        vec3 specular = spec * lights[i].color * intensity;
+        
+        // calculate shadow
+        float shadow = ShadowCalculation(fs_in.FragPosLightSpace);
+        lighting += (ambient + (1.0 - shadow) * (diffuse + specular)) * color;
+    }
+
     FragColor = vec4(lighting, 1.0);
 }
--- Vertex Shaders/ShadowMapping.vs
+++ Vertex Shaders/ShadowMapping.vs
@@ -3,8 +3,6 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
 
-out vec2 TexCoords;
-
 out VS_OUT {
     vec3 FragPos;
     vec3 Normal;
@@ -23,5 +21,5 @@ void main()
     vs_out.Normal = transpose(inverse(mat3(model))) * aNormal;
     vs_out.TexCoords = aTexCoords;
     vs_out.FragPosLightSpace = lightSpaceMatrix * vec4(vs_out.FragPos, 1.0);
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * vec4(vs_out.FragPos, 1.0);
 }
